b4022c406e8acf532da8fa15754abd476931dda3
Hennersxd <lewihe2015@cranleigh.org>
2016-11-07 09:50:33 +0000

    Add files via upload
    Week 9 Preps sir

---
+++ password checker.py
@@ -0,0 +1,6 @@
+password1 = input ("Please enter your password:")
+password2 = input ("Please enter your password again:")
+if password1 == password2:
+    print ("Access Granted")
+else:
+    print ("Access Denied")
---
+++ traffic light.py
@@ -0,0 +1,5 @@
+trafficlight = input ("Please enter the colour of the lights:")
+if trafficlight == "Green":
+    print (" It is safe to cross")
+else:
+    print ("STOP")
