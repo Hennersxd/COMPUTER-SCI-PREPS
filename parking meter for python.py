8a9e5a5a95de2e4f242102a6f15f4a14f60b6eb0
Hennersxd <lewihe2015@cranleigh.org>
2017-01-12 08:35:33 +0000

    Python Parking meter

---
+++ parking meter for python.py
@@ -0,0 +1,21 @@
+# 28Th november 2016
+#This is a program to produce ticket for carparks
+print("Plesase enter amount for the amount of hours you are staying in the carpark")
+print("Please press the button")
+print("enter amount of £5")
+
+totalcash = 0
+
+#we are now going to start the code for adding the money and producing th money
+print("enter car egostration and bay") 
+while totalcash<5:
+            cash=int(input("please enter some money"))
+            totalcash = totalcash+cash
+            if totalcash<5:
+                print("Please enter to the corect value")
+            elif totalcash == 5:
+                print("thank you no change due")
+            elif totalcash>5:
+                print("No change given")
+            print=int(input("this is how much you have enterd")
+            print=(input)
